a788e55f475ac2430339612103c3674476745e30
Kristian Nikolov <92514546+batkriso@users.noreply.github.com>
2026-06-22 21:09:22 +0300

    Created & added sample data & checking queries

---
+++ Readers_Sample_Data_Query.sql
@@ -0,0 +1,11 @@
+INSERT INTO Readers (FirstName, LastName, Email, Phone, RegistrationDate) VALUES
+('Иван', 'Петров', 'ivan.petrov@abv.bg', '0888123456', '2025-01-15'),
+('Мария', 'Димитрова', 'maria.dimitrova@gmail.com', '0878123457', '2025-02-20'),
+('Георги', 'Николов', 'georgi.nikolov@yahoo.com', '0899123458', '2025-03-10'),
+('Елена', 'Иванова', 'elena.ivanova@abv.bg', '0888123459', '2025-04-05'),
+('Петър', 'Георгиев', 'petar.georgiev@gmail.com', '0878123460', '2025-05-12'),
+('София', 'Колева', 'sofia.koleva@abv.bg', '0899123461', '2025-06-01'),
+('Николай', 'Тодоров', 'nikolay.todorov@yahoo.com', '0888123462', '2025-06-15'),
+('Виктория', 'Михайлова', 'viktoriamihaylova@gmail.com', '0878123463', '2025-07-01'),
+('Ангел', 'Стоянов', 'angel.stoyanov@abv.bg', '0899123464', '2025-07-20'),
+('Катерина', 'Николова', 'katerina.nikolova@gmail.com', '0888123465', '2025-08-01');
